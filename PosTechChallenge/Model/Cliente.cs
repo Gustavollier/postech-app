5f9e77fe3f6ef1fd4f18d2db184968ed674fe67f
Guinter Zaffalon <143457091+GuinterZaffalon@users.noreply.github.com>
2026-03-17 19:55:53 -0300

    feat: estrutura modelo cliente

---
+++ PosTechChallenge/Model/Cliente.cs
@@ -0,0 +1,22 @@
+﻿namespace PosTechChallenge.Model
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+
+        public string? CPF { get; set; }        
+
+        public string? CNPJ { get; set; }
+
+        public string? NomeCompleto { get; set; }
+
+        public string? Telefone { get; set; }
+
+        public string? Email { get; set; }
+    }
+}
+    
