b7431e8ab6933e1a1ecd3b8fa232b6d50da3d2a9
Guinter Zaffalon <143457091+GuinterZaffalon@users.noreply.github.com>
2026-03-17 20:06:11 -0300

    feat: estrutura de status modelo e enumerador

---
+++ PosTechChallenge/Model/Status.cs
@@ -0,0 +1,11 @@
+﻿namespace PosTechChallenge.Model
+{
+    public class Status
+    {
+        public int Id { get; set; }
+        public int IdOS { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public int IdFuncionario { get; set; }
+        public int StatusAtual { get; set; }
+    }   
+}
---
+++ PosTechChallenge/Utils/Enums.cs
@@ -0,0 +1,15 @@
+﻿namespace PosTechChallenge.Utils
+{
+    public class Enums
+    {
+        public enum StatusServico
+        {
+            Recebida = 0,
+            EmDiagnostico = 1,
+            AguardandoAprovacao = 2,
+            EmExecucao = 3,
+            Finalizada = 4,
+            Entregue = 5
+        }
+    }
+}
