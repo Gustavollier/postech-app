d274df836fc98984e333d1c38707242c1933a339
Guinter Zaffalon <143457091+GuinterZaffalon@users.noreply.github.com>
2026-03-17 19:57:42 -0300

    feat: estrutura modelo funcionario

---
+++ PosTechChallenge/Model/Funcionario.cs
@@ -0,0 +1,17 @@
+﻿namespace PosTechChallenge.Model
+{
+    public class Funcionario
+    {
+        public int Id { get; set; }
+
+        public string? Nome { get; set; }
+
+        public string? Contato { get; set; }
+
+        public string? CPF { get; set; }
+
+        public int Cargo { get; set; }
+
+        public int ValorHora { get; set; }
+    }
+}
