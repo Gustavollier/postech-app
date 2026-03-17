62261175ac52481ba7c3848e736ecc9b7ac7f11d
Guinter Zaffalon <143457091+GuinterZaffalon@users.noreply.github.com>
2026-03-17 19:59:41 -0300

    feat: estrutura modelo veiculo

---
+++ PosTechChallenge/Model/Veiculo.cs
@@ -0,0 +1,14 @@
+﻿namespace PosTechChallenge.Model
+{
+    public class Veiculo
+    {
+        public int Id { get; set; }
+        public string? Marca { get; set; }
+        public string? Modelo { get; set; }
+        public string? Placa { get; set; }
+        public string? Cor { get; set; }
+        public string? AnoModelo { get; set; }
+        public string? AnoFabricacao { get; set; }
+        public string? KmEntrada { get; set; }
+    }
+}
